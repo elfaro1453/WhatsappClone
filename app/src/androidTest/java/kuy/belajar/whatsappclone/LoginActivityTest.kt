d350edd4fc3fef547ad3314a55a1000f39b0eb5a
elfaro1453 <elfaro1453@gmail.com>
2020-10-23 08:01:17 +0700

    Editing the README to try out git add/commit

---
+++ app/src/androidTest/java/kuy/belajar/whatsappclone/LoginActivityTest.kt
@@ -0,0 +1,20 @@
+package kuy.belajar.whatsappclone
+
+import org.junit.After
+import org.junit.Before
+
+import org.junit.Assert.*
+
+/**
+ * Created by Imam Fahrur Rofi on 22/10/20.
+ */
+class LoginActivityTest {
+
+    @Before
+    fun setUp() {
+    }
+
+    @After
+    fun tearDown() {
+    }
+}
